9a42653374549b808a6b663ba41000b3380b7727
Helder Avila <helderavila2016@gmail.com>
2021-04-18 11:17:13 -0300

    Initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>home | github-profile</title>
+      </Head>
+
+      <main>
+        Hello World
+      </main>
+    </>
+  )
+}
